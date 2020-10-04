c9220b903e974d79c3a14e150a9f8bf0151f22a8
Flor21 <fas-2196@hotmail.com>
2020-10-04 11:32:11 -0300

    Fix in server

--- dawn/dawn/src/main/java/com/dawn/dawn/DawnApplication.java
+++ dawn/dawn/src/main/java/com/dawn/dawn/DawnApplication.java
@@ -3,8 +3,12 @@ package com.dawn.dawn;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @ComponentScan("com.dawn.dawn.controllers")
 @EntityScan("com.dawn.dawn.model")
@@ -15,5 +19,14 @@ public class DawnApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DawnApplication.class, args);
 	}
-
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
+	}
 }
